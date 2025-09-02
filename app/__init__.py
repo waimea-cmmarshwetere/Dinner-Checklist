bba9df24c166cfcb602ad9aaf438fa04c20c68d8
waimea-cmmarshwetere <cmmarshwetere@waimea.school.nz>
2025-09-02 14:48:58 +1200

    Got stuff working for the first time

--- app/__init__.py
+++ app/__init__.py
@@ -29,18 +29,10 @@ init_datetime(app)  # Handle UTC dates in timestamps
 # Home page route
 #-----------------------------------------------------------
 @app.get("/")
-def index():
-    return render_template("pages/planner.jinja")
-
-
-#-----------------------------------------------------------
-# Things page route - Show all the things, and new thing form
-#-----------------------------------------------------------
-@app.get("/planner/")
-def show_all_things():
+def show_all_planner():
     with connect_db() as client:
         # Get all the things from the DB
-        sql = "SELECT id, name FROM planner ORDER BY name ASC"
+        sql = "SELECT * FROM planner ORDER BY name ASC"
         params = []
         result = client.execute(sql, params)
         planner = result.rows
@@ -52,19 +44,19 @@ def show_all_things():
 #-----------------------------------------------------------
 # Thing page route - Show details of a single thing
 #-----------------------------------------------------------
-@app.get("/thing/<int:id>")
-def show_one_thing(id):
+@app.get("/planner/<int:id>")
+def show_one_planner(id):
     with connect_db() as client:
         # Get the thing details from the DB
-        sql = "SELECT id, name, price FROM things WHERE id=?"
+        sql = "SELECT id, name, priority FROM planner WHERE id=?"
         params = [id]
         result = client.execute(sql, params)
 
         # Did we get a result?
         if result.rows:
             # yes, so show it on the page
-            thing = result.rows[0]
-            return render_template("pages/thing.jinja", thing=thing)
+            planner = result.rows[0]
+            return render_template("pages/plan.jinja", plan=planner)
 
         else:
             # No, so show error
@@ -78,20 +70,20 @@ def show_one_thing(id):
 def add_a_thing():
     # Get the data from the form
     name  = request.form.get("name")
-    price = request.form.get("price")
+    priority = request.form.get("priority")
 
     # Sanitise the text inputs
     name = html.escape(name)
 
     with connect_db() as client:
         # Add the thing to the DB
-        sql = "INSERT INTO planner (name, price) VALUES (?, ?)"
-        params = [name, price]
+        sql = "INSERT INTO planner (name, priority) VALUES (?, ?)"
+        params = [name, priority]
         client.execute(sql, params)
 
         # Go back to the home page
-        flash(f"Dinner Plan '{name}' added", "success")
-        return redirect("/planner")
+        flash(f"Family Member '{name}' added", "success")
+        return redirect("/")
 
 
 #-----------------------------------------------------------
@@ -101,12 +93,12 @@ def add_a_thing():
 def delete_a_thing(id):
     with connect_db() as client:
         # Delete the thing from the DB
-        sql = "DELETE FROM Planner.list WHERE id=?"
+        sql = "DELETE FROM planner WHERE id=?"
         params = [id]
         client.execute(sql, params)
 
         # Go back to the home page
         flash("Dinner deleted", "success")
-        return redirect("/planner")
+        return redirect("/")
 
 
